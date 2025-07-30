11822b3e5eee42688d1038a93e5f52dbb6c2a0e5
abel.garcia <ablg11673@gmail.com>
2025-07-30 10:12:46 +0200

    git init

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+import uvicorn
+
+from app import APP, LOGGER, API_PORT, API_IP, LOG_CONFIG
+
+
+if __name__ == '__main__':
+    LOGGER.debug("Starting...")
+    uvicorn.run(
+        APP,
+        host=API_IP,
+        port=API_PORT,
+        reload=False,
+        # log_config=LOG_CONFIG
+    )
+    LOGGER.debug("Ending.")
