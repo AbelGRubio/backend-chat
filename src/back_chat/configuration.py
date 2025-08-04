ab2fe84b17f80b14b8beef40c6949b5070620a50
semantic-release <semantic-release>
2025-08-04 09:40:26 +0000

    1.0.7
    Automatically generated by python-semantic-release

--- src/back_chat/configuration.py
+++ src/back_chat/configuration.py
@@ -15,7 +15,7 @@ from peewee import SqliteDatabase
 from .services import ConnectionManager, RabbitMQManager
 from .utils.logger_api import LoggerApi
 
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 
 LOGGER = LoggerApi("back_chat")
 
